ec5ef35d6eb902fefbd7469e9b0b6128f23c4834
Dex Fitch <fitch.dexter@gmail.com>
2021-09-23 17:55:57 -0400

    More stuff

--- src/components/App.js
+++ src/components/App.js
@@ -7,8 +7,26 @@ import hogs from "../porkers_data";
 
 function App() {
 	const [displayGreased, setDisplayGreased] = useState(false);
+	const [numberOfAwards, setNumberOfAwards] = useState(0);
 
-	let displayHogs = hogs.filter(hog => (displayGreased ? hog.greased : true)); 
+	// state = [
+	// 	{
+	// 		displayGreased: false,
+
+	// 	}
+	// ]
+
+	// ternary!!!!!
+	// let displayHogs = hogs.filter(hog => (displayGreased ? hog.greased : true)); 
+
+	// standard!!!!!
+	let displayHogs = hogs.filter(hog => {
+		if (displayGreased === true) {
+			return hog.greased
+		} else if (displayGreased === false) {
+			return true
+		}
+	})
 
 	return (
 		<div className="App">
